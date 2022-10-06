9ee9218b1661deacb19801bf6add6a23e72680fd
sam-corbett <113055935+sam-corbett@users.noreply.github.com>
2022-10-06 14:29:14 -0400

    init

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2022
+// This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+ function doMathClicked() {
+
+ }
