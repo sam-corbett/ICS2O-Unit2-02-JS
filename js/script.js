533373b689a72b3b30c5676657b62d03574742d2
sam-corbett <113055935+sam-corbett@users.noreply.github.com>
2022-10-11 23:40:58 +0000

    init

--- js/script.js
+++ js/script.js
@@ -3,11 +3,10 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2022
 // This file contains the JS functions for index.html
-
 /**
  * Do basic math.
  */
 function doMathClicked() {
-  document.getElementById("calculate-area").innerhtml =
-    "<p>The Area Of Rectangle is: " + 5 * 3 + "</p>"
+    document.getElementById("calculate-area").innerhtml = "<p>The Area Of Rectangle is: " + (5 * 3) + "</p>"
+  
 }
